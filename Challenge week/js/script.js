423f2358ff1fbc05f3695b454ebba63b1e903a05
sarahmaekelburg <65945459+sarahmaekelburg@users.noreply.github.com>
2020-05-26 10:16:06 +0200

    Added Website prototype

---
+++ Challenge week/js/script.js
@@ -0,0 +1,9 @@
+type="text/javascript"
+
+function monsteralink () {
+  window.location ="../Challenge week/Monstera.html";
+}
+
+function homelink () {
+  window.location = "../Challenge week/index.html";
+}
